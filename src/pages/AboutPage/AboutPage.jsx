c5ce91a34fc818ab8999ffe817750737baa7d028
sinmary86 <sinmary@web.de>
2025-06-21 16:59:19 +0200

    del footer second

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,22 +1,17 @@
 import { AboutContent } from "../../AboutContent/AboutContent";
 import { AboutFeatures } from "../../AboutFeatures/AboutFeatures";
-import { FooterApp } from "../../Footer/FooterApp";
 import { PageBanner } from "../../PageBanner/PageBanner";
 import backgroundImg from '../../assets/two_wom_doc_pat.jpg';
 
 const AboutPage = () => {
      return (
         <div>
-
             <PageBanner
                 background={backgroundImg}
                 title="Votre Coach en Face Fitness et Rajeunissement Naturel"
                 />
-
             <AboutContent />
-            <AboutFeatures />
-            <FooterApp />
-
+            <AboutFeatures />  
         </div> 
      )
 }
