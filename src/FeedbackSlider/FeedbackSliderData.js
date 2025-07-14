d8a0487ebc4db5b3bb3099c164fded9f67ddd224
marinahradway <headway2121@mail.com>
2025-07-14 19:03:09 +0300

    мобильная адаптация

--- src/FeedbackSlider/FeedbackSliderData.js
+++ src/FeedbackSlider/FeedbackSliderData.js
@@ -23,4 +23,30 @@ export const feedbackData = [
     age: 50,
     text: 'Le tester c`est l`approuver. Je recommande l`apprentissage du facefitness avec Natalia Kovaleva. Un outil simple, relaxant et impactant qui agit autant à l`intérieur qu`à l`extérieur!',
 },
+{ 
+    id: 5, 
+    name: 'Nataliya YESINA',
+    age: 54,
+    text: 'J’ai adoré ce petit cours de face fitness ! Coaching pro, bienveillant et motivant. Les exercices sont simples et efficaces, j’ai vite ressenti des effets positifs sur mon visage et mon bien-être. Une méthode naturelle avec une coach passionnée. Je recommande!',
+},
+{ 
+    id: 6, 
+    name: 'Fatima Dahmani',
+    age: 42,
+    text: 'J’ai participé à une séance de yoga du visage en groupe avec Natalia, et je dois dire que c’était une expérience à la fois agréable et efficace. Cette pratique m’a vraiment aidé à détendre les tensions accumulées dans mon visage, notamment au niveau de la mâchoire et du cou, où le stress a tendance à s’accumuler sans que l’on s’en rende compte.',
+},
+{ 
+    id: 7, 
+    name: 'Boistelle Martine',
+    age: 42,
+    text: 'magnifique expérience avec Natalia, très pédagogue et professionnalisme.',
+},
+{ 
+    id: 8, 
+    name: 'Noelie Courbet Asselin',
+    age: 35,
+    text: 'Ce fut pour moi une prise de conscience intéressante et sérieuse des différents muscles de son visage. Un accompagnement très précis et agréable au cours des séances . Une expérience vraiment intéressante.',
+}
+
+
 ];
